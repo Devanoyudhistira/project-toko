246d07dafe19565712d40db8e4312926f697996a
Devanoyudhistira <devanotira@gmail.com>
2024-12-08 22:18:08 +0700

    first project commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function App() {
-  return <h1 className="text-6xl text-blue-500" > lets get started </h1>
+  const [massagedata, setmassagedata] = useState()
+  useEffect(() => {
+    fetch("http://localhost:5000")
+    .then(response => response.json()).then(res => setmassagedata(res.data))
+  },[])
+  async function sendtext(number, status) {
+    let massage = ""
+    if(status === "problem"){
+      massage = "ada masalah segera kembali ke toko"
+    }
+    else if(status === "pending"){
+      massage = "projek sedang dikerjakan"
+    }
+    else{
+      massage = "projek sudah selesai"
+    }
+    const encodedMessage = encodeURIComponent(massage);
+    const whatsappUrl = `https://wa.me/+62${number}?text=${encodedMessage}`;
+
+    window.open(whatsappUrl, '_blank');
+  }
+  return (
+    <div className="flex w-screen h-36 gap-y-3 justify-evenly flex-wrap" >
+
+      {massagedata && massagedata.map((e, i) =>
+        <div key={e.number} className={`border-2 ${e.status === "problem" && "border-red-500"} ${e.status === "pending" && "border-yellow-500"}
+        ${e.status === "done" && "border-green-500"} w-44 h-24 bg-slate-50`} >
+          <h1>{e.kasus}</h1>
+          <button onClick={() => sendtext(e.number, e.status)} >send massage</button>
+          <div className="flex w-24 h-max">
+            <button className="p-2 text-xl border-2 border-yellow-500" onClick={() => setmassagedata(item => item.map((e, idx) => idx === i ? { ...e, status: "pending" } : e))} ></button>
+            <button className="p-2 text-xl border-2 border-red-500" onClick={() => setmassagedata(item => item.map((e, idx) => idx === i ? { ...e, status: "problem" } : e))} ></button>
+            <button className="p-2 text-xl border-2 border-green-500"
+              onClick={() => setmassagedata(item => item.map((e, idx) => idx === i ? { ...e, status: "done" } : e))} ></button>
+
+          </div>
+        </div>)}
+    </div>
+  )
 }
 
 export default App
---
+++ src/backend/datajobs.js
@@ -0,0 +1,65 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 5000;
+
+const data =  [{
+    number: 83845923336,
+    massage: `perbaikan telah selesai segera di ambil`,
+    kasus: "perbaikan laptop",
+    status: "done"
+  }, {
+    number: 89650120007,
+    massage: `ada masalah segera datang ke toko`,
+    kasus: "perbaikan printer",
+    status: "problem"
+  },
+  {
+    number: 83183602751,
+    massage: `perbaikan telah selesai segera di ambil`,
+    kasus: "install ulang",
+    status: "done"
+  },
+  {
+    number: 83183602751,
+    massage: `perbaikan telah selesai segera di ambil`,
+    kasus: "install ulang",
+    status: "done"
+  },
+  {
+    number: 83183602751,
+    massage: `perbaikan telah selesai segera di ambil`,
+    kasus: "install ulang",
+    status: "done"
+  },
+  {
+    number: 83183602751,
+    massage: `perbaikan telah selesai segera di ambil`,
+    kasus: "install ulang",
+    status: "pending"
+  },
+  {
+    number: 83182602751,
+    massage: `perbaikan telah selesai segera di ambil`,
+    kasus: "perbaikan layar laptop",
+    status: "pending"
+  }]
+app.use(
+  cors({
+    origin: '*', // Allow all origins; adjust as needed for security
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({
+    uid: 12345,
+    username: 'devano yudhistira',
+    absen: '',
+    data:data
+  });
+});
+
+app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
+
+export default app; // Export for potential reuse
